65fafe67799f2f4cc248e33794b7f59e37d1f67a
Sreejith Pp <mailz4sreejith@gmail.com>
2017-03-27 17:47:24 +0530

    Added default config file

---
+++ examples/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    redis: {
+        port: 5000,
+        host: '127.0.0.1',
+        options: {
+            // see https://github.com/mranney/node_redis#rediscreateclient
+        }
+    },
+    prefix: 'testing',
+}
